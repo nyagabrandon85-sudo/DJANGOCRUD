d55209c0cca18964d276788b8aa183a32534d138
May-dax <nyagabrandon85@gmail.com>
2026-02-27 16:43:14 +0300

    DJANGOCRUD

--- school/myapp/urls.py
+++ school/myapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add/', views.create_student, name='add'),
     path('students/', views.read_students, name='students'),
     path('delete/<int:id>', views.delete_student, name='delete'),
+    path('update/<int:id>', views.update_student, name='update'),
+    path('register/', views.register_user, name='register'),
 ]
--- school/myapp/views.py
+++ school/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from . forms import Student
 from . forms import StudentForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 #create your views here.
 
 # Create your views here.
@@ -12,6 +14,7 @@ def create_student(request):
     form=StudentForm(request.POST or None)
     if form.is_valid():
             form.save()
+            messages.success(request,'Student created successfully')
             return redirect('students')
     return render(request,'studentform.html',{'form':form})
 #r-read-fetching all recordings from db
@@ -19,8 +22,22 @@ def read_students(request):
     students=Student.objects.all()
     return render(request,'admindashboard.html',{'students':students})
 #update updates a record
+def update_student(request,id):
+    student=get_object_or_404(Student, pk=id)
+    form=StudentForm(request.POST or None, instance=student)
+    if form.is_valid():
+        form.save()
+        return redirect('students')
+    return render(request,'studentform.html',{'form':form})
 #delete-deletes a record
 def delete_student(request,id):
     student=get_object_or_404(Student, pk=id)
     student.delete()
     return redirect('students')
+#create user
+def register_user(request):
+    form=UserCreationForm()
+    
+    return render(request,'register.html',{'form':form})
+
+
